b0083cbcacab48b4063a54dce08e042a02d55cd8
Kayla Fitzgerald <fitzgerald.km11@gmail.com>
2018-10-08 20:50:21 -0400

    exporting practice

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
+// run 'node app.js' in terminal to start running on port 3000
 var express = require('express');
 var app = express();
+var square = require('./square'); // Here we require() the name of the file without the (optional) .js file extension
 
 app.get('/', function(req, res) {
-  res.send('Hello World!');
+  // res.send('Hello World!');
+  res.send('The area of a square with a width of 4 is ' + square.area(4));
 });
 
 app.listen(3000, function() {
   console.log('Example app listening on port 3000!');
 });
 
-// run 'node app.js' in terminal to start running on port 3000
+// console.log('The area of a square with a width of 4 is ' + square.area(4));
---
+++ square.js
@@ -0,0 +1,4 @@
+// exporting module practice
+
+exports.area = function(width) { return width * width; };
+exports.perimeter = function(width) { return 4 * width; };
